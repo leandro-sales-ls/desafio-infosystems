43b6ac2383e96f9136be3f6f347ba288e8a63e1e
Leandro <leandro.sales@acpgroup.com.br>
2020-10-25 20:14:41 -0300

    Ajuste remoção dusk

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment('local', 'testing')) {
-            $this->app->register(DuskServiceProvider::class);
-        }
+        //
     }
 
     /**
